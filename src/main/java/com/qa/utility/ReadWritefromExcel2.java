4f1cc179b1e665d5b8c354bfa186026dd62d3792
Pankaj <hanumant@gmail.com>
2025-07-17 23:16:02 +0530

    For jenkins

--- src/main/java/com/qa/utility/ReadWritefromExcel2.java
+++ src/main/java/com/qa/utility/ReadWritefromExcel2.java
@@ -14,8 +14,14 @@ import java.util.*;
 
 public class ReadWritefromExcel2 {
 
-	public static String filePath = "D:\\archetype-fitgoodies-quickstart\\src\\main\\java\\com\\qa\\testData\\Book1.xlsx";
-
+    public static String filePath = System.getProperty("user.dir") + java.io.File.separator +
+            "src" + java.io.File.separator +
+            "main" + java.io.File.separator +
+            "java" + java.io.File.separator +
+            "com" + java.io.File.separator +
+            "qa" + java.io.File.separator +
+            "testData" + java.io.File.separator +
+            "Book1.xlsx";
    public String sheet1 = "Sheet1";
 
     public String readData(String sheetName, int rowNumber, int cellNumber) {
